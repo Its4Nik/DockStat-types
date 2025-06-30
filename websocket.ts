e029d99dcfbadf80156532ee57dd5f969c696332
Its4Nik <info@itsnik.de>
2025-06-30 13:57:44 +0200

    Update typings

--- websocket.ts
+++ websocket.ts
@@ -1,15 +1,16 @@
 interface stackSocketMessage {
-	message?: string;
-	type?: "stack-progress" | "stack-error" | "stack-status" | "stack-removed";
-	data?: stackSocketData;
+  timestamp: Date;
+  message?: string;
+  type?: "stack-progress" | "stack-error" | "stack-status" | "stack-removed";
+  data?: stackSocketData;
 }
 
 interface stackSocketData {
-	stack_id: number;
-	message: string;
-	action?: string;
-	status?: string;
-	timestamp?: string;
+  stack_id: number;
+  message: string;
+  action?: string;
+  status?: string;
+  timestamp?: string;
 }
 
 export type { stackSocketMessage };
